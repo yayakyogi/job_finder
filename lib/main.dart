223e6ce9f57353c5d7c7fed33e09cb5b39572e69
yayakyogi <yayaktaka@gmail.com>
2022-01-14 23:11:34 +0700

    fix theme fontFamily

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:job_finder/pages/home_page.dart';
+import 'package:job_finder/pages/splash_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,7 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      theme: ThemeData(fontFamily: 'DM-Sans'),
+      home: Splash(),
     );
   }
 }
