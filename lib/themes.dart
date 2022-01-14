ef745faa338a8a0857cc86823ab551d6f0392848
yayakyogi <yayaktaka@gmail.com>
2022-01-14 23:09:38 +0700

    fix theme color and fontWeight

---
+++ lib/themes.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+const Color whiteColor = Color(0xffFFFFFF);
+const Color blackColor = Color(0xff081D43);
+const Color blueColor = Color(0xff5078E1);
+const Color greyColor = Color(0xffE5E5E5);
+const Color grayColor = Color(0xffF8FAFD);
+
+const TextStyle fw_500 = TextStyle(fontWeight: FontWeight.w500);
+const TextStyle fw_600 = TextStyle(fontWeight: FontWeight.w600);
+const TextStyle fw_700 = TextStyle(fontWeight: FontWeight.w700);
